595cc42d7c59620450e29b44f8ea7f57890a8a6c
André Luiz <andre.doctors@gmail.com>
2024-06-14 17:42:35 -0400

    feat: Database seeding

--- src/main/java/com/api/dscatalog/services/CategoryService.java
+++ src/main/java/com/api/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.api.dscatalog.entities.Category;
 import com.api.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Transactional(readOnly = true)
     public List<Category> getAllCategories() {
         return categoryRepository.findAll();
     }
